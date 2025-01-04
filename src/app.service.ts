6a663a31bab1f37450d0c35a0490dcb76d85f24a
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2025-01-04 16:22:49 -0300

    Att casesensitive Error

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,16 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     const date = new Date();
-    const hello = () => {
-      if (date.getHours() >= 0 && date.getHours() < 12) {
-        return 'Bom dia!';
-      } else if (date.getHours() >= 12 && date.getHours() < 18) {
-        return 'Boa tarde!';
-      } else {
-        return 'Boa noite!';
-      }
-    };
-
-    return `Olá! ${hello()} hoje é ${date.toLocaleDateString('pt-BR')}, e são ${date.toLocaleTimeString('pt-BR')}.`;
+    return `Olá! hoje é ${date.toLocaleDateString('pt-BR')}`;
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,12 +10,32 @@ export class ProductsService {
 
   async create(createProductDto: CreateProductDto) {
     try {
+      // verifica se existe um produto com o mesmo nome (case insensitive)
+      const existingProduct = await this.prisma.products.findFirst({
+        where: {
+          name: {
+            equals: createProductDto.name,
+            mode: 'insensitive',
+          },
+        },
+      });
+
+      if (existingProduct) {
+        throw new HttpException(
+          'Um produto com esse nome já existe',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
       const product = await this.prisma.products.create({
         data: createProductDto,
       });
       return product;
     } catch (error) {
-      console.log(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new HttpException(
@@ -69,13 +89,41 @@ export class ProductsService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
     try {
+      //verifica se já existe outro produto com esse nome
+      if (updateProductDto.name) {
+        const existingProduct = await this.prisma.products.findFirst({
+          where: {
+            AND: [
+              {
+                name: {
+                  equals: updateProductDto.name,
+                  mode: 'insensitive',
+                },
+              },
+              {
+                id: {
+                  not: id, // Exclui o produto atual da verificação para nao dar erro de duplicidade
+                },
+              },
+            ],
+          },
+        });
+        if (existingProduct) {
+          throw new HttpException(
+            'Um produto com esse nome já existe',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+      }
       const product = await this.prisma.products.update({
         where: { id },
         data: updateProductDto,
       });
       return product;
     } catch (error) {
-      console.log(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new HttpException(
@@ -83,9 +131,17 @@ export class ProductsService {
             HttpStatus.BAD_REQUEST,
           );
         }
+        if (error.code === 'P2025') {
+          throw new HttpException(
+            'Produto não encontrado',
+            HttpStatus.NOT_FOUND,
+          );
+        }
       }
-      console.log(error);
-      throw new HttpException('Erro ao editar o item', HttpStatus.BAD_REQUEST);
+      throw new HttpException(
+        'Erro ao editar o produto',
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
 
